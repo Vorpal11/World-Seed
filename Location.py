b5abcb49cfb9a03433621a8e675677bd7ab06dd2
GGonnerman <gaston95g@gmail.com>
2020-08-11 16:17:15 -0500

    Implemented immutable location

--- Location.py
+++ Location.py
@@ -1,25 +1,31 @@
 class Location:
+
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
 
     # Returns a tuple containing the grid locations
     def get_location(self):
-        return (self.x, self.y)
+        return (self._x, self._y)
 
     # Returns a tuple containing the coord locations
     def get_coord_location(self):
-        return (self.x * 16, self.y * 16)
+        return (self._x * 16, self._y * 16)
 
     def __str__(self):
-        return f"({self.x}, {self.y})"
+        return f"({self._x}, {self._y})"
 
     def __add__(self, other):
         if isinstance(other, Location):
-            self.x += other.x
-            self.y += other.y
+            self._x += other._x
+            self._y += other._y
+            return Location(self._x + other._x, self._y + other._y)
         else:
             if len(other) != 2: raise Exception('Error adding to Location')
-            self.x += other[0]
-            self.y += other[1]
-        return self
+            return Location(self._x + other[0], self._y + other[1])
